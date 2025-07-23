36db3635f547adfac789e16e71c1b66c6cd16c23
liaoyusong <liaoyusong@cowarobot.com>
2025-07-23 18:28:10 +0800

    do some

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut num = 1;
+    let mut n = n;
+    while n > 0 {
+        num = num * n;
+        n -= 1;
+    }
+    num
 }
 
 #[cfg(test)]
